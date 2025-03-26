4c0bc9a71c0d3a4b84ff74582a57e8a69c28ccfa
rishi0810 <rishi.1003raj@gmail.com>
2025-03-26 13:57:39 +0530

    Updating domain

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://frquery.vercel.app", "http://localhost:5173"],
     credentials: true
   })
 );
